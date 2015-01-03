f96359e72172a274647d77b9dd971045a3788c71
HahaTTpro <hahattpro@gmail.com>
2015-01-03 09:39:13 +0700

    to debug phase
    done coding, now it time to run on genymotion

--- app/src/main/java/com/example/tt/notebook/Edit_Note_Activity.java
+++ app/src/main/java/com/example/tt/notebook/Edit_Note_Activity.java
@@ -5,8 +5,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class Edit_Note_Activity extends ActionBarActivity {
@@ -16,7 +26,7 @@ public class Edit_Note_Activity extends ActionBarActivity {
     private Button SaveButton;
     private String NOTE_NAME ;// no .txt
                                //only name
-
+    private String FILE_NAME;//FILENAME =NOTE_NAME+".txt"
 
     //////////
 
@@ -28,11 +38,63 @@ public class Edit_Note_Activity extends ActionBarActivity {
         SaveButton = (Button) findViewById(R.id.SaveNoteButton);
         Bundle data = getIntent().getExtras();
         NOTE_NAME = data.get("name").toString();
-        
+        FILE_NAME= NOTE_NAME+".txt";
+        ReadFile();
+
+        SaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WriteFile();//save a note
+                ReadFile();//update it on screen
+            }
+        });
+    }
+
+    public void ReadFile()
+    {
+        String tmp;
+        String out;
+        try {
+            FileInputStream in = openFileInput(FILE_NAME);
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder builder = new StringBuilder();
+            while ((tmp = bufferedReader.readLine())!=null)
+            {
+                builder.append(tmp);
+                builder.append("\n");
+            }
+            out = builder.toString();
+            NoteContent.setText(out);
+        }
+        catch (FileNotFoundException e)
+        {
+
+        }
+        catch (IOException e)
+        {
+
+        }
 
     }
 
+    public void WriteFile()
+    {
+        String tmp;
 
+        try{
+            FileOutputStream out = openFileOutput(FILE_NAME,0);
+            OutputStreamWriter writer = new OutputStreamWriter(out);
+            writer.write(NoteContent.getText().toString());
+            writer.close();
+        }
+        catch (FileNotFoundException e){
+
+        }
+        catch (IOException e){
+            //do nothing
+        }
+    }
 
 
 
